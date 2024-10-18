5406f4cb7068377b7b8b2378f2b63613c54af8f1
YesithValdes <ayvaldes@unicauca.edu.co>
2024-10-18 09:52:09 -0500

    rfasfsada

--- mp/src/app/crud/crud.component.ts
+++ mp/src/app/crud/crud.component.ts
@@ -1,16 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-crud',
   standalone: true,
-  imports: [],
-  template: `
-    <p>
-      crud works!
-    </p>
-  `,
+  imports: [CommonModule,ReactiveFormsModule],
+  templateUrl: './crud.component.html',
   styleUrl: './crud.component.css'
 })
 export class CrudComponent {
 
-}
+  resultado!: string;
+  constructor(){
+
+  }
+
+  formularioLogin = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.maxLength(40)]),
+    password: new FormControl('', [Validators.required, Validators.maxLength(250)])
+  });
+
+  submit() {
+    if (this.formularioLogin.valid)
+      this.resultado = "Todos los datos son válidos";
+    else
+      this.resultado = "Hay datos inválidos en el formulario";
+  }
+
+}   
