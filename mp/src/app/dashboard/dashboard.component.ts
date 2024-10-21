cc6cc0741a4db6e809d1de97f139be8d2b9efec9
YesithValdes <ayvaldes@unicauca.edu.co>
2024-10-21 13:29:12 -0500

    fjkadsjfl

--- mp/src/app/dashboard/dashboard.component.ts
+++ mp/src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { switchMap } from 'rxjs/operators';
-import { Observable, of } from 'rxjs';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -13,22 +9,5 @@ import { CommonModule } from '@angular/common';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-  user$: Observable<any> | undefined; // Use Observable for user data
 
-  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore) {
-    this.user$ = this.afAuth.authState.pipe(
-      switchMap(user => {
-        if (user) {
-          // Get user data from Firestore based on user UID
-          return this.afs.doc(`usuarios/${user.uid}`).valueChanges();
-        } else {
-          return of(null); // Return null if user is not logged in
-        }
-      })
-    );
-  }
-
-  logout() {
-    this.afAuth.signOut();
-  }
 }
